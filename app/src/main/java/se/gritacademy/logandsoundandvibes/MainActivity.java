406bbee1829e8b799897b44c786ce8f9253938f5
Alrik He <alrik_he@hotmail.com>
2024-09-02 10:11:48 +0200

    Initial commit

---
+++ app/src/main/java/se/gritacademy/logandsoundandvibes/MainActivity.java
@@ -0,0 +1,61 @@
+package se.gritacademy.logandsoundandvibes;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Log.d("alrik", "onCreate: ");
+
+        findViewById(R.id.button).setOnClickListener( (e)->{
+            vibrate();
+        });
+
+        MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.random);
+        mp.start();
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+    }
+
+
+    void vibrate() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
+    }
+
+}
